14ad4bb5264159c9b4c4fe6419e6cdcc9bee7a01
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-03-01 16:43:16 -0300

    relacionamento Professor <> Department

--- src/main/java/com/project/professor/allocation/fabio1/entity/Professor.java
+++ src/main/java/com/project/professor/allocation/fabio1/entity/Professor.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Professor {
@@ -12,11 +14,19 @@ public class Professor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(nullable = false)
 	private String name;
 
 	@Column(length = 11, unique = true, nullable = false)
 	private String cpf;
+	
+	@Column(name = "department_Id", nullable = false)
 	private Long departmentId;
+	
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "department_Id", updatable = false, insertable = false, nullable = false)
+	private Department Depart;
 
 	public Long getId() {
 		return id;
