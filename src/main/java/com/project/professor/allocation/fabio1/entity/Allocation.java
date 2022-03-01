ba72f325fabdcc5d72c13a74abdfa2a2e1441d1f
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-03-01 16:04:28 -0300

    Mudando dayOfWeek para String

--- src/main/java/com/project/professor/allocation/fabio1/entity/Allocation.java
+++ src/main/java/com/project/professor/allocation/fabio1/entity/Allocation.java
@@ -3,7 +3,10 @@ package com.project.professor.allocation.fabio1.entity;
 import java.time.DayOfWeek;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -16,6 +19,8 @@ public class Allocation {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 50)
 	private DayOfWeek day;
 	@Temporal(TemporalType.TIME)
 	private Date start;
