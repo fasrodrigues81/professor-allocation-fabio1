910c9e78143df5582f40e037bbf07dbfc2669d7e
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-03-01 14:14:46 -0300

    construção das Classes pacote entity

---
+++ src/main/java/com/project/professor/allocation/fabio1/entity/Course.java
@@ -0,0 +1,24 @@
+package com.project.professor.allocation.fabio1.entity;
+
+public class Course {
+
+	private Long id;
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
