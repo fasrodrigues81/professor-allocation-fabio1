97f932a8583db3cfe066f5126c72f67efb9802ad
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-03-09 22:38:22 -0300

    add Cors e Swagger

---
+++ src/main/java/com/project/professor/allocation/fabio1/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.project.professor.allocation.fabio1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.project.professor.allocation.fabio1.ProfessorAllocationFabio1Application;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+	
+	 @Bean
+	    public Docket api() {
+	        return new Docket(DocumentationType.SWAGGER_2).select()
+	                .apis(RequestHandlerSelectors.basePackage(ProfessorAllocationFabio1Application.class.getPackage().getName()))
+	                .paths(PathSelectors.any())
+	                .build()
+	                .useDefaultResponseMessages(false)
+	                .apiInfo(getApiInfo());
+	    }
+
+	    private ApiInfo getApiInfo() {
+	        return new ApiInfoBuilder()
+	                .title("Professor Allocation")
+	                .description("Professor Allocation Rest Server")
+	                .version("0.0.1-SNAPSHOT")
+	                .build();
+	    }
+	
+
+}
