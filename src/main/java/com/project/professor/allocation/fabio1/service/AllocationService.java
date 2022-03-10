b2ee14945c2e035d51ae6ab3d715978a2ac7a615
Fabio Rodrigues <fasrodrigues81@gmail.com>
2022-03-10 18:08:53 -0300

    CRUD AllocationService

--- src/main/java/com/project/professor/allocation/fabio1/service/AllocationService.java
+++ src/main/java/com/project/professor/allocation/fabio1/service/AllocationService.java
@@ -1,9 +1,12 @@
 package com.project.professor.allocation.fabio1.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.project.professor.allocation.fabio1.entity.Course;
+import com.project.professor.allocation.fabio1.entity.Professor;
 import com.project.professor.allocation.fabio1.entity.Allocation;
 import com.project.professor.allocation.fabio1.repository.AllocationRepository;
 
@@ -11,10 +14,15 @@ import com.project.professor.allocation.fabio1.repository.AllocationRepository;
 public class AllocationService {
 
 	private final AllocationRepository allocationRepository;
+	private final ProfessorService professorService;
+	private final CourseService courseService;
 
-	public AllocationService(AllocationRepository allocationRepository) {
+	public AllocationService(AllocationRepository allocationRepository, ProfessorService professorService,
+			CourseService courseService) {
 		super();
 		this.allocationRepository = allocationRepository;
+		this.professorService = professorService;
+		this.courseService = courseService;
 	}
 
 	// CRUD READ all
@@ -26,16 +34,21 @@ public class AllocationService {
 
 	// CRUD READ by ID
 	public Allocation findById(Long id) {
-
-		// Optional<Allocation> optional = allocationRepository.findById(id);
-		// Allocation allocation = optional.orElse(null)
-		// return allocation;
+		Optional<Allocation> optional = allocationRepository.findById(id);
+		Allocation allocation = optional.orElse(null);
 		return allocationRepository.findById(id).orElse(null);
+	}
+
+	public List<Allocation> findByProfessor(Long professorId) {
+		return allocationRepository.findByProfessorId(professorId);
+	}
+
+	public List<Allocation> findByCourse(Long courseId) {
+		return allocationRepository.findByCourseId(courseId);
 	}
 
 	// CRUD CREATE
 	public Allocation create(Allocation allocation) {
-
 		allocation.setId(null);
 		Allocation allocationNew = allocationRepository.save(allocation);
 		return allocationNew;
@@ -51,7 +64,6 @@ public class AllocationService {
 		} else {
 			return null;
 		}
-
 	}
 
 	// CRUD DELET by ID
